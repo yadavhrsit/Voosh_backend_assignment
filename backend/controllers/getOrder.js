139a8a1d0ba8999f5597835a3c7155ccb16e8ded
Harshit Yadav <inbox.harshityadav@gmail.com>
2023-01-31 18:53:15 +0530

    Ref Fixed

--- backend/controllers/getOrder.js
+++ backend/controllers/getOrder.js
@@ -6,17 +6,9 @@ async function getOrder(req, res) {
         user: req.body.user_id,
     })
         .populate("user", "name phone -_id")
+        .populate("item", "name price img -_id")
         .select({ item: 1, user: 1, sub_total: 1, _id: 0 });
-    orders[0].item.forEach((element) => {
-        ProductModal.findById(element, function (err, data) {
-            if (err) {
-                return res.send(err);
-            }
-            else {
-                console.log(data)
-            }
-        })
-    })
+
     res.send(orders)
 }
 module.exports = getOrder;
